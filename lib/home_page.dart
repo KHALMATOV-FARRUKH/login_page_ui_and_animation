dc1fc7b204591abcb6e8acbac89fa98894131b88
KHALMATOV-FARRUKH <khalmatov.farrukh@gmail.com>
2023-03-28 20:48:32 +0500

    Added dependence: simple_animations: ^4.2.0
    Created:
    fade_animation.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_page_ui_and_animation/animation/fade_animation.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,23 +11,172 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            height: 400,
-              child: Stack(
-                children: [
-                  Positioned(child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/"),
+            height: 390,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: -40,
+                  height: 400,
+                  width: width + 20,
+                  child: FadeAnimation(
+                    1,
+                    Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/background.png"),
+                          fit: BoxFit.fill,
+                        ),
                       ),
                     ),
-                  ),),
-                ],
-              ),
+                  ),
+                ),
+                Positioned(
+                  height: 400,
+                  width: width + 20,
+                  child: FadeAnimation(
+                    1.5,
+                    Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/background-2.png"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // const SizedBox(
+          //   height: 20,
+          // ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FadeAnimation(
+                  1.5,
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                        color: Color.fromRGBO(49, 39, 79, 1),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                FadeAnimation(
+                  1.7,
+                  Container(
+                    // padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromRGBO(196, 135, 198, .3),
+                            blurRadius: 20,
+                            offset: Offset(0, 10),
+                            // spreadRadius: 1,
+                          ),
+                        ]),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(color: Colors.grey.shade200),
+                            ),
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Username",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(color: Colors.grey.shade300),
+                            ),
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Password",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                FadeAnimation(
+                  1.7,
+                  Center(
+                    child: Text(
+                      "Forgot Password",
+                      style: TextStyle(
+                        color: Color.fromRGBO(196, 135, 198, 1),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                FadeAnimation(
+                  1.9,
+                  Container(
+                    height: 60,
+                    margin: EdgeInsets.symmetric(horizontal: 60),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Color.fromRGBO(49, 39, 79, 1),
+                    ),
+                    child: Center(
+                        child: Text(
+                      "Login",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17),
+                    )),
+                  ),
+                ),
+                SizedBox(height: 15),
+                FadeAnimation(
+                  2,
+                  Center(
+                    child: Text(
+                      "Create Account",
+                      style: TextStyle(
+                        color: Color.fromRGBO(49, 39, 79, 0.6),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_page_ui_and_animation/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
